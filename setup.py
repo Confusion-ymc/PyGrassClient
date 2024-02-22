e019bef7d3fb65dce95bdb59ddf8570485934931
Confusion-YMC <32094374+Confusion-ymc@users.noreply.github.com>
2024-02-22 19:13:08 +0800

    update test.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 
 with open("README.md", "r") as f:
     long_description = f.read()
-setup(name='PyGrass',  # 包名
+setup(name='PyGrassClient',  # 包名
       version='1.0.0',  # 版本号
       description='Get Grass Python Package',
       long_description=long_description,
       author='confusion',
       author_email='1107238486@qq.com',
-      url='https://github.com/Confusion-ymc/PyGrass',
+      url='https://github.com/Confusion-ymc/PyGrassClient',
       install_requires=[
           'websocket-client==1.7.0',
           'loguru==0.7.2',
