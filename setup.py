c270f4197aa5ab5d9fee46aa1c0ed43737c11b7a
confusion <yangmingcheng@matrixone.io>
2024-03-28 17:46:30 +0800

    修改安装依赖

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "PyGrassClient", "__version__.py"), "r", encoding="utf-8") as f:
     exec(f.read(), about)
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(name='PyGrassClient',  # 包名
       version=about["__version__"],  # 版本号
       description=about["__description__"],
@@ -19,11 +22,7 @@ setup(name='PyGrassClient',  # 包名
       author=about["__author__"],
       author_email=about["__author_email__"],
       url=about["__url__"],
-      install_requires=[
-          'websocket-client==1.7.0',
-          'loguru==0.7.2',
-          'python-socks==2.4.4'
-      ],
+      install_requires=install_requires,
       license=about["__license__"],
       packages=find_packages(),
       platforms=["all"],
